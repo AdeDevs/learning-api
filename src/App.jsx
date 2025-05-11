30260b2d7dc432ff609f82322e1b7984a8c30e71
AdeDevs <adeyemiakinyemi01@gmail.com>
2025-05-11 11:31:12 +0100

    updt

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { useEffect, useState } from 'react'
-import './App.css'
 import ProfilePage from './Test'
 
 function App() {
   const [hamburgerMenu, setHamburgerMenu] = useState(false)
   const [count, setCount] = useState(0)
-  // const [gender, setGender] = useState('')
 
   const toggleMenu = () => {
     setHamburgerMenu(!hamburgerMenu)
@@ -31,17 +29,6 @@ function App() {
           <li onClick={toggleMenu}>Subarashi</li>
         </ul>
       </nav>
-
-      {/* <main>
-        <div className="card">
-          <h1>{count}</h1>
-          <button onClick={() => { if (count < 10) setCount(count + 1) }}>Increase</button>
-          <button onClick={() => { if (count > 0) setCount(count - 1) }}>Decrease</button>
-        </div>
-        <div className="profile">
-          <p> {gender ? gender : "loading"}</p>
-        </div>
-      </main> */}
       <main>
         <ProfilePage />
       </main>
